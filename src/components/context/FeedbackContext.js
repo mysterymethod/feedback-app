a162ca18a3ecae374095165e7ae84c6cd3d694a7
Pranoy Chandra <chandra.pranay@yahoo.in>
2022-01-10 15:09:42 +0530

    adding new feedback to backend and added proxy for localhost3000

--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
@@ -15,7 +14,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // F E T C H   F E E D B A C K  F R O M   B A C K E N D
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback`);
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
     const data = await response.json();
     setFeedback(data);
     setIsLoading(false);
@@ -31,9 +30,18 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // A D D   F E E D B A C K
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback)
+    })
+
+    const data = await response.json()
+    setFeedback([data, ...feedback]);
   };
 
   // E D I T   F E E D B A C K
